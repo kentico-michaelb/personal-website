2e889d70451456ef8645e2ae8e57d7cc1dcbfba5
kentico-michaelb <michaelb@kentico.com>
2022-03-14 16:23:16 -0400

    Adds posts route

---
+++ gatsby-node.js
@@ -0,0 +1,25 @@
+exports.createPages = async function ({ actions, graphql }) {
+    const { data } = await graphql(`
+    query {
+        allKontentItemPost {
+          edges {
+            node {
+              elements {
+                url {
+                  value
+                }
+              }
+            }
+          }
+        }
+      }
+    `)
+    data.allKontentItemPost.edges.forEach(edge => {
+      const slug = edge.node.elements.url.value
+      actions.createPage({
+        path: `/posts/${slug}`,
+        component: require.resolve(`./src/templates/post.js`),
+        context: { slug: slug },
+      })
+    })
+  }
