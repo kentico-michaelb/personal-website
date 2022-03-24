30ce4c3bed8f18b6a5ec9c3aed842013d23dc13e
kentico-michaelb <michaelb@kentico.com>
2022-03-24 07:52:55 -0400

    Extends post query

--- src/hooks/use-all-posts.js
+++ src/hooks/use-all-posts.js
@@ -1,58 +1,93 @@
 import { graphql, useStaticQuery } from "gatsby";
 
 export const useAllPosts = () => {
-    const { allKontentItemExternalArticle, allKontentItemPost } = useStaticQuery(graphql`
-    query {
-            allKontentItemExternalArticle(sort: {fields: elements___date___value}) {
-                nodes {
-                elements {
-                    title {
-                    value
-                    }
-                    date {
-                    value(formatString: "MMMM Do, YYYY")
-                    }
-                    host {
-                    value {
-                        name
-                    }
-                    }
+    const { allKontentItemExternalArticle, allKontentItemPost } =
+      useStaticQuery(graphql`
+        query {
+          allKontentItemExternalArticle(
+            sort: { fields: elements___date___value }
+          ) {
+            nodes {
+              elements {
+                title {
+                  value
                 }
-                system {
-                    id
-                    type
+                date {
+                  value(formatString: "MMMM Do, YYYY")
                 }
+                host {
+                  value {
+                    name
+                  }
+                }
+              }
+              system {
+                id
+                type
+              }
             }
-            }
-            allKontentItemPost(sort: {fields: elements___date___value}) {
-                nodes {
-                elements {
-                    category {
-                    value {
-                        name
-                    }
-                    }
-                    date {
-                        value(formatString: "MMMM Do, YYYY")
-                    }
-                    title {
-                    value
-                    }
-                    topic {
-                    value {
-                        name
-                    }
-                    }
+          }
+          allKontentItemPost(sort: { fields: elements___date___value }) {
+            nodes {
+              elements {
+                category {
+                  value {
+                    name
+                  }
                 }
-                system {
-                    id
-                    type
+                date {
+                  value(formatString: "MMMM Do, YYYY")
                 }
+                title {
+                  value
+                }
+                topic {
+                  value {
+                    name
+                  }
+                }
+                image {
+                  value {
+                    system {
+                      codename
+                      id
+                      type
+                    }
+                    ... on kontent_item_media {
+                      id
+                      system {
+                        codename
+                        id
+                        type
+                      }
+                      elements {
+                        alt {
+                          value
+                        }
+                        asset {
+                          value {
+                            description
+                            height
+                            name
+                            size
+                            type
+                            url
+                            width
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              system {
+                id
+                type
+              }
             }
+          }
         }
-    }
-    
-    `)
+      `);
     return [allKontentItemExternalArticle, allKontentItemPost]
 }
 
