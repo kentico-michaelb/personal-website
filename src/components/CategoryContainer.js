1f88b00ce67710aaaa8782df0b4bee2d4f7dcc98
kentico-michaelb <michaelb@kentico.com>
2022-03-25 15:42:50 -0400

    Adds anchor link support

--- src/components/CategoryContainer.js
+++ src/components/CategoryContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import SubduedLogo from '../components/SubduedLogo'
 import { useAllCategory } from '../hooks/use-all-category'
+import { AnchorLink } from 'gatsby-plugin-anchor-links'
 
 export default function CategoryContainer(){
     const categories = useAllCategory()
@@ -13,15 +14,17 @@ export default function CategoryContainer(){
             {categories &&
                 categories.nodes.map(category => {
                 return (
-                    <div className='h-36 md:h-80 w-full bg-custom-dark-gray 
+                    // <AnchorLink to={`/${category.elements.url.value}#${category.elements.url.value}`}
+                    <AnchorLink to={`/${category.elements.url.value}#${category.elements.url.value}`}
+                        stripHash
+                        key={category.system.id}
+                        className='h-36 md:h-80 w-full bg-custom-dark-gray 
                         bg-opacity-30 hover:bg-opacity-100
                         cursor-pointer flex items-center justify-center'>
-                        <Link 
-                            to={`/${category.elements.url.value}`} 
-                            key={category.system.id}>
+                        <div id={category.system.codename}>
                             <h1 className='text-white'>{category.elements.title.value}</h1>
-                        </Link>
-                    </div>)
+                        </div>
+                    </AnchorLink>)
                 })
             }
         </div>
